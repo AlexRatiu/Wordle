38d08ce56621e8657bbe4dbe1bd40483c4c7f564
AlexRatiu <117086170+AlexRatiu@users.noreply.github.com>
2024-03-11 16:56:47 +0200

    init

--- src/components/Game.js
+++ src/components/Game.js
@@ -525,6 +525,7 @@ function Game() {
 	const [wordHistory, setWordHistory] = useState([...Array(6)].fill(''));
 	const [currentTry, setCurrentTry] = useState(0);
 	const [matrix, setMatrix] = useState([...Array(6)].fill(Array(5).fill("#121213")));
+	const [keyColor, setKeyColor] = useState([...Array(26)].fill('#818384'));
 
 	// Handlers
 	const handleType = (char) => {	
@@ -554,6 +555,15 @@ function Game() {
 		});
 	};
 
+	const updateKeyboard = (a, color) => {
+		setKeyColor(prevKeyColor => {
+			const newKeyColor = [...prevKeyColor];
+			const index = a.charCodeAt(0);
+			newKeyColor[index-65] = color;
+			return newKeyColor;
+		});
+	};
+
 	const handleCheckWord = () =>{
 		var statusList = ["Give another word a go", "Try a different term", "Experiment with a new phrase", "Test out another word", "Explore a fresh choice", "Seek a different option", "Try a varied approach", "Aim for a different way of saying it", "Give a new word a shot", "Look for an alternative word"]
 		if(wordHistory[currentTry].length === 5){
@@ -570,12 +580,19 @@ function Game() {
 			}
 
 			for(let a = 0; a < 5; a++){
+				if(keyColor[wordHistory[currentTry][a].charCodeAt(0)-65] !== "#538d4e" && keyColor[wordHistory[currentTry][a].charCodeAt(0)-65] !== "#b59f3b"){
+					updateKeyboard(wordHistory[currentTry][a], "#3a3a3c");
+				}
 				for(let b = 0; b < 5; b++){
 					if(wordHistory[currentTry][a] === wordOfTheDay[b]){
 						updateMatrix(currentTry,a,"#b59f3b");
+						if(keyColor[wordHistory[currentTry][a].charCodeAt(0)-65] !== "#538d4e"){
+							updateKeyboard(wordHistory[currentTry][a], "#b59f3b");
+						}
 					}
 					if(wordHistory[currentTry][a] === wordOfTheDay[a]){		
-						updateMatrix(currentTry,a,"#538d4e");		
+						updateMatrix(currentTry,a,"#538d4e");
+						updateKeyboard(wordHistory[currentTry][a], "#538d4e");
 					}
 				}
 			}
@@ -619,37 +636,37 @@ function Game() {
 						const char = String.fromCharCode(97 + index);
 						return <button className='keyboardButton' onClick={() => handleType(char)}>{char}</button>
 					})} */}
-					<button className='keyboardButton' onClick={() => handleType('Q')}>Q</button>
-					<button className='keyboardButton' onClick={() => handleType('W')}>W</button>
-					<button className='keyboardButton' onClick={() => handleType('E')}>E</button>
-					<button className='keyboardButton' onClick={() => handleType('R')}>R</button>
-					<button className='keyboardButton' onClick={() => handleType('T')}>T</button>
-					<button className='keyboardButton' onClick={() => handleType('Y')}>Y</button>
-					<button className='keyboardButton' onClick={() => handleType('U')}>U</button>
-					<button className='keyboardButton' onClick={() => handleType('I')}>I</button>
-					<button className='keyboardButton' onClick={() => handleType('O')}>O</button>
-					<button className='keyboardButton' onClick={() => handleType('P')}>P</button>
+					<button className='keyboardButton' style={{backgroundColor: keyColor["Q".charCodeAt(0)-65]}} onClick={() => handleType('Q')}>Q</button>
+					<button className='keyboardButton' style={{backgroundColor: keyColor["W".charCodeAt(0)-65]}} onClick={() => handleType('W')}>W</button>
+					<button className='keyboardButton' style={{backgroundColor: keyColor["E".charCodeAt(0)-65]}} onClick={() => handleType('E')}>E</button>
+					<button className='keyboardButton' style={{backgroundColor: keyColor["R".charCodeAt(0)-65]}} onClick={() => handleType('R')}>R</button>
+					<button className='keyboardButton' style={{backgroundColor: keyColor["T".charCodeAt(0)-65]}} onClick={() => handleType('T')}>T</button>
+					<button className='keyboardButton' style={{backgroundColor: keyColor["Y".charCodeAt(0)-65]}} onClick={() => handleType('Y')}>Y</button>
+					<button className='keyboardButton' style={{backgroundColor: keyColor["U".charCodeAt(0)-65]}} onClick={() => handleType('U')}>U</button>
+					<button className='keyboardButton' style={{backgroundColor: keyColor["I".charCodeAt(0)-65]}} onClick={() => handleType('I')}>I</button>
+					<button className='keyboardButton' style={{backgroundColor: keyColor["O".charCodeAt(0)-65]}} onClick={() => handleType('O')}>O</button>
+					<button className='keyboardButton' style={{backgroundColor: keyColor["P".charCodeAt(0)-65]}} onClick={() => handleType('P')}>P</button>
 				</div>
 				<div className='gameKeyboardRow2'>
-					<button className='keyboardButton' onClick={() => handleType('A')}>A</button>
-					<button className='keyboardButton' onClick={() => handleType('S')}>S</button>
-					<button className='keyboardButton' onClick={() => handleType('D')}>D</button>
-					<button className='keyboardButton' onClick={() => handleType('F')}>F</button>
-					<button className='keyboardButton' onClick={() => handleType('G')}>G</button>
-					<button className='keyboardButton' onClick={() => handleType('H')}>H</button>
-					<button className='keyboardButton' onClick={() => handleType('J')}>J</button>
-					<button className='keyboardButton' onClick={() => handleType('K')}>K</button>
-					<button className='keyboardButton' onClick={() => handleType('L')}>L</button>
+					<button className='keyboardButton' style={{backgroundColor: keyColor["A".charCodeAt(0)-65]}} onClick={() => handleType('A')}>A</button>
+					<button className='keyboardButton' style={{backgroundColor: keyColor["S".charCodeAt(0)-65]}} onClick={() => handleType('S')}>S</button>
+					<button className='keyboardButton' style={{backgroundColor: keyColor["D".charCodeAt(0)-65]}} onClick={() => handleType('D')}>D</button>
+					<button className='keyboardButton' style={{backgroundColor: keyColor["F".charCodeAt(0)-65]}} onClick={() => handleType('F')}>F</button>
+					<button className='keyboardButton' style={{backgroundColor: keyColor["G".charCodeAt(0)-65]}} onClick={() => handleType('G')}>G</button>
+					<button className='keyboardButton' style={{backgroundColor: keyColor["H".charCodeAt(0)-65]}} onClick={() => handleType('H')}>H</button>
+					<button className='keyboardButton' style={{backgroundColor: keyColor["J".charCodeAt(0)-65]}} onClick={() => handleType('J')}>J</button>
+					<button className='keyboardButton' style={{backgroundColor: keyColor["K".charCodeAt(0)-65]}} onClick={() => handleType('K')}>K</button>
+					<button className='keyboardButton' style={{backgroundColor: keyColor["L".charCodeAt(0)-65]}} onClick={() => handleType('L')}>L</button>
 				</div>
 				<div className='gameKeyboardRow3'>
 					<button className='keyboardButtonCheck' onClick={() => handleCheckWord()}>Enter</button>
-					<button className='keyboardButton' onClick={() => handleType('Z')}>Z</button>
-					<button className='keyboardButton' onClick={() => handleType('X')}>X</button>
-					<button className='keyboardButton' onClick={() => handleType('C')}>C</button>
-					<button className='keyboardButton' onClick={() => handleType('V')}>V</button>
-					<button className='keyboardButton' onClick={() => handleType('B')}>B</button>
-					<button className='keyboardButton' onClick={() => handleType('N')}>N</button>
-					<button className='keyboardButton' onClick={() => handleType('M')}>M</button>
+					<button className='keyboardButton' style={{backgroundColor: keyColor["Z".charCodeAt(0)-65]}} onClick={() => handleType('Z')}>Z</button>
+					<button className='keyboardButton' style={{backgroundColor: keyColor["X".charCodeAt(0)-65]}} onClick={() => handleType('X')}>X</button>
+					<button className='keyboardButton' style={{backgroundColor: keyColor["C".charCodeAt(0)-65]}} onClick={() => handleType('C')}>C</button>
+					<button className='keyboardButton' style={{backgroundColor: keyColor["V".charCodeAt(0)-65]}} onClick={() => handleType('V')}>V</button>
+					<button className='keyboardButton' style={{backgroundColor: keyColor["B".charCodeAt(0)-65]}} onClick={() => handleType('B')}>B</button>
+					<button className='keyboardButton' style={{backgroundColor: keyColor["N".charCodeAt(0)-65]}} onClick={() => handleType('N')}>N</button>
+					<button className='keyboardButton' style={{backgroundColor: keyColor["M".charCodeAt(0)-65]}} onClick={() => handleType('M')}>M</button>
 					<button className='keyboardButtonDelete' onClick={() => handleDeleteLastChar()}>⌫</button>
 				</div>
 			</div>
